73f47e3a03e89e016917d5540a352950b1809ca7
kwin <15951815688@163.com>
2020-07-15 14:21:30 +0800

    修改了用户注册的中文乱码问题

--- gmall-user-web/src/main/java/com/njupt/gmall/user/controller/UserController.java
+++ gmall-user-web/src/main/java/com/njupt/gmall/user/controller/UserController.java
@@ -6,7 +6,6 @@ import com.njupt.gmall.bean.UmsMemberReceiveAddress;
 import com.njupt.gmall.service.UmsMemberReceiveAddressService;
 import com.njupt.gmall.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -43,21 +42,6 @@ public class UserController {
         return phoneResult;
     }
 
-
-    @RequestMapping("register")
-    public ModelAndView register( HttpServletRequest request, ModelMap modelMap){
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        String phone = request.getParameter("phone");
-        UmsMember umsMember = new UmsMember();
-        umsMember.setUsername(username);
-        umsMember.setPassword(password);
-        umsMember.setPhone(phone);
-        userService.register(umsMember);
-        ModelAndView mv = new ModelAndView("redirect:http://localhost:8085/index?ReturnUrl=http://localhost:8083/index");
-        return mv;
-    }
-
     @RequestMapping("index")
     public ModelAndView index(HttpServletRequest request){
         String username = request.getParameter("username");
@@ -65,7 +49,12 @@ public class UserController {
         String phone = request.getParameter("phone");
         ModelAndView mv = null;
         if(username != null){
-            String url = "redirect:http://localhost:8080/register?username=" + username + "&password=" + password + "&phone=" + phone;
+            UmsMember umsMember = new UmsMember();
+            umsMember.setUsername(username);
+            umsMember.setPassword(password);
+            umsMember.setPhone(phone);
+            userService.register(umsMember);
+            String url = "redirect:http://localhost:8085/index?ReturnUrl=http://localhost:8083/index";
             mv = new ModelAndView(url);
         }else{
             mv = new ModelAndView("index");
