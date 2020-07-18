ef267ecfaba0bc2180ec604084501353751be8a1
kwin <15951815688@163.com>
2020-07-18 22:25:29 +0800

    备注Redis连接池的各个参数

--- gmall-service-util/src/main/java/com/njupt/gmall/util/RedisUtil.java
+++ gmall-service-util/src/main/java/com/njupt/gmall/util/RedisUtil.java
@@ -13,10 +13,15 @@ public class RedisUtil {
     //初始化一个连接池
     public void initPool(String host,int port ,int database){
         JedisPoolConfig poolConfig = new JedisPoolConfig();
+        //最大连接数
         poolConfig.setMaxTotal(200);
+        //最大空闲连接数
         poolConfig.setMaxIdle(50);
+        //连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
         poolConfig.setBlockWhenExhausted(true);
+        //获取连接时的最大等待毫秒数(如果设置为阻塞时BlockWhenExhausted),如果超时就抛异常, 小于零:阻塞不确定的时间）
         poolConfig.setMaxWaitMillis(10*1000);
+        //在获取连接的时候检查有效性
         poolConfig.setTestOnBorrow(true);
         jedisPool=new JedisPool(poolConfig,host,port,20*1000);
     }
